87badbbff119c56ebdbc0ca67890be9aa0383be9
pedrogms82 <pedrogms82@gmail.com>
2019-01-11 13:10:26 +0100

    añado imagen

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from "rxjs";
 import { map, filter } from 'rxjs/operators';
@@ -10,17 +10,17 @@ import { map, filter } from 'rxjs/operators';
 })
 export class ApiService {
 
-  readonly rootUrl = 'http://app.ayudapyme.es';
+  readonly rootUrl = 'http://auto.arysoft.es/velneo';
   public reqHeader = new HttpHeaders({ 'Content-Type': 'application/x-www-urlencoded','No-Auth':'True','Access-Control-Allow-Origin': '*' });
 
 
   constructor(private http: HttpClient) { }
 
   public getFamilias()  {
-  return this.http.get('http://app.ayudapyme.es/vERP_2_dat_dat/v1/fam_m?api_key=fam');
+  return this.http.get(this.rootUrl+'/vERP_2_dat_dat/v1/fam_m?api_key=fam');
   }
 
   public getArticuloFamiliaNum(FamNum)  {
-  return this.http.get('http://app.ayudapyme.es/vERP_2_dat_dat/v1/art_m?filter%5Bfam%5D='+FamNum+'&api_key=art');
+  return this.http.get(this.rootUrl+'/vERP_2_dat_dat/v1/art_m?filter%5Bfam%5D='+FamNum+'&api_key=art');
   }
 }
